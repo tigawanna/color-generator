191358a1f248e3144339de0581f7e0473443eb4a
tigawanna <denniskinuthiaw@gamil.com>
2023-05-27 20:06:29 +0300

    vercel,json

--- src/pages/colors/Colors.tsx
+++ src/pages/colors/Colors.tsx
@@ -1,13 +1,12 @@
 import { ColorsList } from "@/my-ui/color_editor/ColorsList";
-import { ColorsEditors } from "@/my-ui/colors/ColorsEditors";
+
 
 interface ColorsProps {}
 
 export function Colors({}: ColorsProps) {
   return (
     <div className="w-full h-full flex items-center justify-center">
-       {/* <ColorsEditors/> */}
-       <ColorsList/>
+      <ColorsList/>
     </div>
   );
 }
